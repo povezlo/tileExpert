a343729380af388555a72ea7e6a8a7990327a243
povezlo <aliester.crowly@gmail.com>
2021-02-15 19:41:37 +0200

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SocialBarComponent } from './shared/components/social-bar/social-bar.component';
+import { SearchBlockComponent } from './shared/components/search-block/search-block.component';
+import {ClickOutsideDirective} from './directives/click-out.directive';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SocialBarComponent
+    SocialBarComponent,
+    SearchBlockComponent,
+    ClickOutsideDirective
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
