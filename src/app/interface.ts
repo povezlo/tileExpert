0a4c1131a93b87395c396232f3f5042a0075b502
povezlo <aliester.crowly@gmail.com>
2021-02-16 15:11:51 +0200

    add subscription

---
+++ src/app/interface.ts
@@ -0,0 +1,12 @@
+export interface FilterCheckBoxList {
+  id: string;
+  checkbox: string;
+  isActive: boolean;
+  mobile: boolean;
+}
+
+export interface SelectedList {
+  value: string;
+  display: string;
+  mobile: boolean;
+}
--- src/app/shared/components/search-block/search-block.component.ts
+++ src/app/shared/components/search-block/search-block.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { fadeInLeft, zoomIn } from 'ng-animate';
-import { Observable } from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import {FilterCheckBoxList, SelectedList} from '../../../interface';
 
 @Component({
   selector: 'app-search-block',
@@ -14,39 +15,45 @@ import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
     trigger('fadeInLeft', [transition('* => *', useAnimation(fadeInLeft))])
   ]
 })
-export class SearchBlockComponent implements OnInit {
+export class SearchBlockComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+
   isHandset$: Observable<BreakpointState> = this.breakpointObserver.observe(['(max-width: 860px)']);
   value = '';
   openedSearch = false;
   myAuthorIsActive: false;
-  filterCheckBox = [
+  zoomIn: any;
+  fadeInLeft: any;
+  sortfilterCheckBox: FilterCheckBoxList[] = [];
+  sortSelected: SelectedList[] = [];
+
+  filterCheckBox: FilterCheckBoxList[] = [
     { id: 'checkbox1', checkbox: 'участник', isActive: false, mobile: false },
     { id: 'checkbox2', checkbox: 'в заголовке', isActive: true, mobile: false },
     { id: 'checkbox3', checkbox: 'в контактах', isActive: false, mobile: true },
     { id: 'checkbox4', checkbox: 'строгий поиск', isActive: true, mobile: false }
   ];
-  public selected = [
+  public selected: SelectedList[] = [
     { value: 'rel', display: 'релевантности', mobile: true },
     { value: 'date', display: 'дате', mobile: false },
     { value: 'title', display: 'названию', mobile: false },
     { value: 'price', display: 'цене', mobile: false }
   ];
-  sortfilterCheckBox = [];
-  sortSelected = [];
-  zoomIn: any;
-  fadeInLeft: any;
+
   constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
-    this.isHandset$.subscribe(e => {
+    this.subscription = this.isHandset$.subscribe(e => {
       if (!e.matches) {
-        this.sortfilterCheckBox = this.filterCheckBox.filter(e => e.mobile === false);
-        this.sortSelected = this.selected.filter(e => e.mobile === false);
+        this.sortfilterCheckBox = this.filterCheckBox.filter(elem => elem.mobile === false);
+        this.sortSelected = this.selected.filter(elem => elem.mobile === false);
       } else {
         this.sortfilterCheckBox = this.filterCheckBox;
         this.sortSelected = this.selected;
       }
     });
   }
-
+ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+}
 }
